6cac78f1babd9b6eecc589f51599d27240101c34
Oganezov <oganezovkv@mail.ru>
2016-10-27 12:07:05 +0300

    Первоначальный коммит клиента ПФХД v.0.0.1

---
+++ Form1.Designer.cs
@@ -0,0 +1,250 @@
+﻿namespace ClientPFHD
+{
+    partial class FormClientPFHD
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.loginTB = new System.Windows.Forms.TextBox();
+            this.passwordTB = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.uriCB = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxXML = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.pathFileTB = new System.Windows.Forms.TextBox();
+            this.buttonOpenXML = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.buttonSend = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.methodCB = new System.Windows.Forms.ComboBox();
+            this.filenameTB = new System.Windows.Forms.TextBox();
+            this.tBresponse = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // loginTB
+            // 
+            this.loginTB.Location = new System.Drawing.Point(100, 25);
+            this.loginTB.Name = "loginTB";
+            this.loginTB.Size = new System.Drawing.Size(212, 20);
+            this.loginTB.TabIndex = 0;
+            this.loginTB.Text = "integrationUser";
+            // 
+            // passwordTB
+            // 
+            this.passwordTB.Location = new System.Drawing.Point(100, 51);
+            this.passwordTB.Name = "passwordTB";
+            this.passwordTB.Size = new System.Drawing.Size(212, 20);
+            this.passwordTB.TabIndex = 2;
+            this.passwordTB.Text = "Cit159632";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(56, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Логин";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(49, 54);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Пароль";
+            // 
+            // uriCB
+            // 
+            this.uriCB.FormattingEnabled = true;
+            this.uriCB.Items.AddRange(new object[] {
+            "incomingPlan",
+            "expensePlan",
+            "finActivityPlan",
+            "extraValue"});
+            this.uriCB.Location = new System.Drawing.Point(100, 77);
+            this.uriCB.Name = "uriCB";
+            this.uriCB.Size = new System.Drawing.Size(212, 21);
+            this.uriCB.TabIndex = 5;
+            this.uriCB.Text = "IncomePlan";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(68, 80);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(26, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "URI";
+            // 
+            // textBoxXML
+            // 
+            this.textBoxXML.Location = new System.Drawing.Point(52, 155);
+            this.textBoxXML.Multiline = true;
+            this.textBoxXML.Name = "textBoxXML";
+            this.textBoxXML.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBoxXML.Size = new System.Drawing.Size(577, 170);
+            this.textBoxXML.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(49, 139);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(32, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "XML:";
+            // 
+            // pathFileTB
+            // 
+            this.pathFileTB.Location = new System.Drawing.Point(139, 108);
+            this.pathFileTB.Name = "pathFileTB";
+            this.pathFileTB.Size = new System.Drawing.Size(331, 20);
+            this.pathFileTB.TabIndex = 9;
+            // 
+            // buttonOpenXML
+            // 
+            this.buttonOpenXML.Location = new System.Drawing.Point(15, 106);
+            this.buttonOpenXML.Name = "buttonOpenXML";
+            this.buttonOpenXML.Size = new System.Drawing.Size(100, 23);
+            this.buttonOpenXML.TabIndex = 10;
+            this.buttonOpenXML.Text = "Выбрать XML";
+            this.buttonOpenXML.UseVisualStyleBackColor = true;
+            this.buttonOpenXML.Click += new System.EventHandler(this.buttonOpenXML_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // buttonSend
+            // 
+            this.buttonSend.Location = new System.Drawing.Point(295, 369);
+            this.buttonSend.Name = "buttonSend";
+            this.buttonSend.Size = new System.Drawing.Size(75, 23);
+            this.buttonSend.TabIndex = 11;
+            this.buttonSend.Text = "Отправить";
+            this.buttonSend.UseVisualStyleBackColor = true;
+            this.buttonSend.Click += new System.EventHandler(this.buttonSend_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(49, 340);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(89, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Метод отправки";
+            // 
+            // methodCB
+            // 
+            this.methodCB.FormattingEnabled = true;
+            this.methodCB.Items.AddRange(new object[] {
+            "/API/Docs/Edubpl/Put"});
+            this.methodCB.Location = new System.Drawing.Point(139, 332);
+            this.methodCB.Name = "methodCB";
+            this.methodCB.Size = new System.Drawing.Size(490, 21);
+            this.methodCB.TabIndex = 13;
+            this.methodCB.Text = "/API/Docs/Edubpl/Put";
+            // 
+            // filenameTB
+            // 
+            this.filenameTB.Location = new System.Drawing.Point(493, 109);
+            this.filenameTB.Name = "filenameTB";
+            this.filenameTB.Size = new System.Drawing.Size(136, 20);
+            this.filenameTB.TabIndex = 14;
+            // 
+            // tBresponse
+            // 
+            this.tBresponse.Location = new System.Drawing.Point(681, 40);
+            this.tBresponse.Multiline = true;
+            this.tBresponse.Name = "tBresponse";
+            this.tBresponse.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.tBresponse.Size = new System.Drawing.Size(503, 359);
+            this.tBresponse.TabIndex = 15;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(693, 20);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(37, 13);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "Ответ";
+            // 
+            // FormClientPFHD
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1196, 411);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.tBresponse);
+            this.Controls.Add(this.filenameTB);
+            this.Controls.Add(this.methodCB);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.buttonSend);
+            this.Controls.Add(this.buttonOpenXML);
+            this.Controls.Add(this.pathFileTB);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBoxXML);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.uriCB);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.passwordTB);
+            this.Controls.Add(this.loginTB);
+            this.Name = "FormClientPFHD";
+            this.Text = "Клиент интеграции ВС ВУЗа - МГУДТ с АСУ ПФХД";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox loginTB;
+        private System.Windows.Forms.TextBox passwordTB;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox uriCB;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxXML;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox pathFileTB;
+        private System.Windows.Forms.Button buttonOpenXML;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button buttonSend;
+        private System.Windows.Forms.ComboBox methodCB;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox filenameTB;
+        private System.Windows.Forms.TextBox tBresponse;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Cache;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace ClientPFHD
+{
+    public partial class FormClientPFHD : Form
+    {
+        public FormClientPFHD()
+        {
+            InitializeComponent();
+        }
+
+        public static HttpWebResponse PostMethod(string postedData, string postUrl, string boundary, byte[] boundarybytes, NameValueCollection nvc, string filename, string pathfile, FileStream fileStream)
+        {
+            //string boundary = "--" + DateTime.Now.Ticks.ToString("x");
+            //byte[] boundarybytes = System.Text.Encoding.ASCII.GetBytes("\r\n--" + boundary + "\r\n");
+
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(postUrl);
+            request.ContentType = "multipart/form-data; boundary=" + boundary;
+            //request.ContentType = "multipart/form-data; boundary=--8ba90904-d8f4-4a64-9e80-8e6e87983e6f--";
+            request.Method = "POST";
+            //request.Credentials = CredentialCache.DefaultCredentials;
+            request.Host = "pfhd.edu.ru";
+            request.KeepAlive = false;
+            
+            using (Stream rs = request.GetRequestStream())
+            {
+                string formdataTemplate = "Content-Disposition: form-data; name=\"{0}\"\r\n\r\n{1}";
+
+                foreach (string key in nvc.Keys)
+                {
+                    rs.Write(boundarybytes, 0, boundarybytes.Length);
+                    string formitem = string.Format(formdataTemplate, key, nvc[key]);
+                    byte[] formitembytes = System.Text.Encoding.UTF8.GetBytes(formitem);
+                    rs.Write(formitembytes, 0, formitembytes.Length);
+
+                }
+                rs.Write(boundarybytes, 0, boundarybytes.Length);
+
+                string headerTemplate = "Content-Disposition: form-data; name=\"{0}\"; filename=\"{1}\"\r\nContent-Type: {2}\r\n\r\n";
+                string header = string.Format(headerTemplate, "content", filename, "text/xml");
+                byte[] headerbytes = System.Text.Encoding.UTF8.GetBytes(header);
+                rs.Write(headerbytes, 0, headerbytes.Length);
+
+                //FileStream fileStream = new FileStream(pathfile + "/" + filename, FileMode.Open, FileAccess.Read);
+                byte[] buffer = new byte[4096];
+                int bytesRead = 0;
+                while ((bytesRead = fileStream.Read(buffer, 0, buffer.Length)) != 0)
+                {
+                    rs.Write(buffer, 0, bytesRead);
+                }
+                fileStream.Close();
+                byte[] trailer = System.Text.Encoding.ASCII.GetBytes("\r\n--" + boundary + "--\r\n");
+                rs.Write(trailer, 0, trailer.Length);
+                rs.Close();
+                HttpWebResponse wresp = null;
+                try
+                {
+                    wresp = (HttpWebResponse)request.GetResponse();
+                    //Stream stream2 = wresp.GetResponseStream();
+                    return wresp;
+                    //StreamReader reader2 = new StreamReader(stream2,Encoding.UTF8);
+                    //return string.Format("File uploaded, server response is: {0}", reader2.ReadToEnd());
+
+                }
+                catch (WebException wex)
+                {
+                    if (wresp != null)
+                    {
+                        wresp.Close();
+                        wresp = null;
+                        if (wex.Status == WebExceptionStatus.ProtocolError)
+                            return (HttpWebResponse)wex.Response;                   
+                    }
+                    return (HttpWebResponse)wex.Response;
+
+                }
+                finally
+                {
+                    request = null;
+                }
+            }
+            
+
+
+            /*UTF8Encoding encoding = new UTF8Encoding();
+            var bytes = encoding.GetBytes(postedData);
+
+            request.ContentLength = bytes.Length;
+            HttpRequestCachePolicy noCachePolicy = new HttpRequestCachePolicy(HttpRequestCacheLevel.NoCacheNoStore);
+            request.CachePolicy = noCachePolicy;
+
+            using (var newStream = request.GetRequestStream())
+            {
+                newStream.Write(bytes, 0, bytes.Length);
+                newStream.Close();
+            }
+            try
+            {
+                return (HttpWebResponse)request.GetResponse();
+            }
+            catch (WebException wex)
+            {
+                if (wex.Status == WebExceptionStatus.ProtocolError)
+                    return (HttpWebResponse)wex.Response;
+                else
+                    throw;
+            }*/
+            
+        }
+        private void buttonOpenXML_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                System.IO.StreamReader sr = new System.IO.StreamReader(openFileDialog1.FileName);
+                string sxml = sr.ReadToEnd().ToString();
+                // string newsxml = "";
+                //string newsxml = ProcessData(sxml);
+                // this.textBox3.Text = newsxml;
+                this.textBoxXML.Text = sxml;
+                sr.Close();
+                string put = openFileDialog1.FileName;
+                this.filenameTB.Text = openFileDialog1.SafeFileName;
+                // this.textBox7.Text = put;
+                this.pathFileTB.Text = put;
+            }
+            string s = this.textBoxXML.Text;
+            if (String.IsNullOrWhiteSpace(s))
+                s = "<Root></Root>";
+            XElement xElement = null;
+            try
+            {
+                xElement = XElement.Load(new StringReader(s));
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Неверный XML");
+                return;
+            }
+        }
+
+        private void buttonSend_Click(object sender, EventArgs e)
+        {
+
+            string postedData,host, ApiUrl;
+            string boundaryText = "--" + DateTime.Now.Ticks.ToString("x");
+            byte[] boundarybytes = System.Text.Encoding.ASCII.GetBytes("\r\n--" + boundaryText + "\r\n");
+
+            host = "http://pfhd.edu.ru";
+            ApiUrl = host + this.methodCB.Text;
+            //boundaryText = "--8ba90904-d8f4-4a64-9e80-8e6e87983e6f";
+            postedData = boundaryText+ "\r\n";
+            postedData += "Content-Disposition: form-data; name=\"AC.Login\"\r\n\r\n";
+            postedData += this.loginTB.Text + "\r\n";
+            postedData += boundaryText + "\r\n";
+            postedData += "Content-Disposition: form-data; name =\"AC.Password\"\r\n\r\n";
+            postedData += this.passwordTB.Text + "\r\n";
+            postedData += boundaryText + "\r\n";
+            postedData += "Content-Disposition: form-data; name =\"URI\"\r\n\r\n";
+            postedData += this.uriCB.Text + "\r\n";
+            postedData += boundaryText + "\r\n";
+            postedData += "Content-Disposition: form-data; name =\"content\"; filename=\""+ this.filenameTB.Text  + "\"\r\n\r\n";
+            postedData += "Content-Type: text/xml\r\n\r\n";
+            postedData += this.textBoxXML.Text;
+            postedData += boundaryText + "--\r\n";
+
+            NameValueCollection myCol = new NameValueCollection();
+            myCol.Add("AC.Login", this.loginTB.Text);
+            myCol.Add("AC.Password", this.passwordTB.Text);
+            myCol.Add("URI", this.uriCB.Text);
+
+            FileStream fileStream = new FileStream(openFileDialog1.FileName, FileMode.Open, FileAccess.Read);
+
+            var response = PostMethod(postedData,ApiUrl,boundaryText,boundarybytes,myCol,this.filenameTB.Text, this.pathFileTB.Text,fileStream);
+            if (response != null)
+            {
+                var strreader = new StreamReader(response.GetResponseStream(), Encoding.UTF8);
+                var responseToString = strreader.ReadToEnd();
+                tBresponse.Text = responseToString;
+            }
+            
+        }
+    }
+}
